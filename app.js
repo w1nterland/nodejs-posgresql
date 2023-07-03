caca3ca88171bd99d0e3515864300afa44f9197b
w1nterland <wy.wynn@protonmail.com>
2023-07-03 15:48:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,4 +17,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/quotes', quotesRouter);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+app.get('/req/*', (req, res) => {
+    var url = req.url.replace("/req/", "");
+    console.log(url)
+    request(
+        { url: `https://aiotube.deta.dev/video/${url}` },
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: err.message });
+            }
+
+            res.json(JSON.parse(body));
+        }
+    )
+});
+
 module.exports = app;
