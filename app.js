58077f98e912867ed8b58b32c81366c4dc513023
w1nterland <wy.wynn@protonmail.com>
2023-07-03 15:51:30 +0300

    a

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const request = require('request');
 
-var indexRouter = require('./routes/index');
-var quotesRouter = require('./routes/quotes');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/quotes', quotesRouter);
+const app = express();
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -36,5 +22,3 @@ app.get('/req/*', (req, res) => {
         }
     )
 });
-
-module.exports = app;
